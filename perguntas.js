851776137ad1181a2f0764dc9a2c0ed919e35fda
IIssaacc08 <eurick.campos@escola.pr.gov.br>
2025-08-14 08:35:29 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,22 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Futebol',
+    'Qual ano o flamengo foi fundado?',
+    '1895'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Futebol',
+    'Quantas vezes o Brasil foi campeão mundial?',
+    '5'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Futebol',
+    'Qual ano o Corinthians foi fundado?',
+    '1910'
 )
     criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Futebol',
+    'Qual ano Seedorf chegou ao Botafogo?',
+    '2012'
 )
